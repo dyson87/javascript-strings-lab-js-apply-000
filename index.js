39ab8902cea314e0f9bbb213661f0e2b61be5050
Courtney Dyson <courtneydyson87@gmail.com>
2019-06-09 01:42:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson!";
 
 var greetSpecialGuest = "Hello," + topic + specialGuest + "!";
 
-var topic = "space";
+var topic = "Let's talk about space";
 
 var conversation = `${topic}`;
